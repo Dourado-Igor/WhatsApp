6212cc2d1516b6d5f1fd2ed902b1c9270ea20688
Dourado1983 <igorgonzalezdourado@gmail.com>
2023-01-18 16:53:21 +0100

    pidiendo.mensajes

---
+++ index.js
@@ -0,0 +1,30 @@
+window.onload=function(){
+    console.log("cargando");
+    $("#btnEnviar").click(()=>{
+        console.log("enviando");
+        let id_usuario_origen=$("#idUsuarioOrigen").val();
+        let id_usuario_destino=$("#idUsuarioDestino").val();
+        let msg=$("#mensaje").val();
+        let datos={
+            "id_usuario_origen":id_usuario_origen,
+            "id_usuario_destino":id_usuario_destino,
+            "mensaje":msg
+        }
+        $.ajax({
+            type:"POST",
+            url:"http://localhost/mvc/api/new",
+            data:datos,
+            success:(dat)=>{
+                dat=JSON.parse(dat);
+                $("#respuesta").text(dat.msg);
+                $("#idUsuarioOrigen").val("");
+                $("#idUsuarioDestino").val("");
+                $("#mensaje").val("");
+            },
+            error:(err)=>{
+                $("#respuesta").text(err);
+            }            
+        });
+
+    })
+}
